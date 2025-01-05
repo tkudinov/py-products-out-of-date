24140fb7d433a16edfce1e03f055ce919baa5b3f
Taras <t.kudinov90@gmail.com>
2025-01-05 18:51:08 +0200

    Solution with passed autotests for reviewing.

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+from unittest import mock
+from app.main import outdated_products
+import datetime
+import pytest
+
+
+@pytest.fixture()
+def products() -> list:
+    yield [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+
+def test_outdated_products(products) -> None:
+    with mock.patch("datetime.date") as mocked_date:
+        mocked_date.today.return_value = datetime.date(2022, 2, 2)
+        outdated_products(products)
+        mocked_date.assert_called_once()
