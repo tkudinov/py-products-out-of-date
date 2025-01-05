02e1932b591a99532d51909bfcc6e12483bed837
Taras <t.kudinov90@gmail.com>
2025-01-05 19:44:15 +0200

    Another solution with passed autotests after first review.

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,23 @@
+import unittest
 from unittest import mock
-from app.main import outdated_products
 import datetime
-import pytest
+from app.main import outdated_products
 
+@mock.patch("datetime.date")
+def test_outdated_products(mock_date) -> None:
+    class CustomDate(datetime.date):
+        @classmethod
+        def today(cls):
+            return datetime.date(2025, 1, 2)
 
-@pytest.fixture()
-def products() -> list:
-    yield [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
+    mock_date.side_effect = CustomDate
+    products = [
+        {"name": "Milk", "expiration_date": datetime.date(2024, 12, 31)},
+        {"name": "Eggs", "expiration_date": datetime.date(2025, 1, 5)},
+        {"name": "Cheese", "expiration_date": datetime.date(2025, 1, 1)},
     ]
 
+    expected_result = ["Milk", "Cheese"]
+    result = outdated_products(products)
 
-def test_outdated_products(products: list) -> None:
-    with mock.patch("datetime.date") as mocked_date:
-        mocked_date.today.return_value = datetime.date(2022, 2, 2)
-        outdated_products(products)
-        mocked_date.assert_called_once()
+    assert result == expected_result
